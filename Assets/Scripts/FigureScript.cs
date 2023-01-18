136e9f6b6214640bb883032d672adb0f47c579b4
Crayz12 <arsenii.solovev12@gmail.com>
2023-01-18 13:23:05 +0300

    stable

--- Assets/Scripts/FigureScript.cs
+++ Assets/Scripts/FigureScript.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class FigureScript : MonoBehaviour
 {
-    public string[] figClasses = {"Melee", "Lancer", "Shooter"};
+    public string[] figClasses = { "Melee", "Lancer", "Shooter" };
     public string Figure_Class;
     public bool IsKing;
     public int Power;
+    public bool MovementAP;
+    public bool AttackAP;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/Figure_Movement.cs
+++ Assets/Scripts/Figure_Movement.cs
@@ -10,19 +10,13 @@ using static UnityEngine.UI.Image;
 [RequireComponent(typeof(Outline))]
 public class Figure_Movement : MonoBehaviour
 {
-    private Dictionary<int, GameObject> NumToFigure = new Dictionary<int, GameObject>();
-
     private Outline outline;
-
-    public GameObject Shooter, Lancer, Melee, Shooter1, Lancer1, Melee1;
 
     public GameObject ChosenFigure, ChosenTile, CurrentTile, hex_start, hex_end;
 
-    public Transform Grid;
-    
     [Header("Radius of finding available tiles   13 is recomended")]
     public int Radius = 13;
-    
+
     [Header("Default and available for movement tile materials")]
     public Material NewMaterial, DefaultMaterial;
 
@@ -31,68 +25,20 @@ public class Figure_Movement : MonoBehaviour
     void Start()
     {
         outline = GetComponent<Outline>();
-        SetToStartPositions();
-       
-    }
-    void SetToStartPositions()
-    {
-        NumToFigure.Add(1, Lancer);
-        NumToFigure.Add(2, Melee);
-        NumToFigure.Add(3, Shooter);
-        NumToFigure.Add(4, Lancer1);
-        NumToFigure.Add(5, Melee1);
-        NumToFigure.Add(6, Shooter1);
         Player1Turn = true;
-
-        Instantiate(NumToFigure[DataHolder.hero1]);
-        Instantiate(NumToFigure[DataHolder.hero2]);
-        Instantiate(NumToFigure[DataHolder.hero3]);
-        Instantiate(NumToFigure[DataHolder.hero4]);
-        Instantiate(NumToFigure[DataHolder.hero5]);
-        Instantiate(NumToFigure[DataHolder.hero1t2]);
-        Instantiate(NumToFigure[DataHolder.hero2t2]);
-        Instantiate(NumToFigure[DataHolder.hero3t2]);
-        Instantiate(NumToFigure[DataHolder.hero4t2]);
-        Instantiate(NumToFigure[DataHolder.hero5t2]);
-        GameObject[] P1Figures = GameObject.FindGameObjectsWithTag("Player1Figure");
-        GameObject[] P2Figures = GameObject.FindGameObjectsWithTag("Player2Figure");
-        foreach(GameObject p in P1Figures)
-        {
-            p.transform.SetParent(Grid);
-        }
-        foreach(GameObject p in P2Figures)
-        {
-            p.transform.SetParent(Grid);
-        }
-        float[] x_spots = { 1.731953f, 0.8659766f, 0f, -0.8659766f, -1.731953f, -2.59793f };
-        float[] z_spots = { -4.5f, 3 };
-        for (int i = 0; i < P1Figures.Length; i++)
-        {
-            Vector3 pos = P1Figures[i].transform.position;
-            pos.x = x_spots[i] * 4;
-            pos.z = z_spots[0] * 4;
-            Quaternion rot = P1Figures[i].transform.rotation;
-            rot.y = 0f;
-            P1Figures[i].transform.position = pos;
-            P1Figures[i].transform.rotation = rot;
-        }
-        for (int i = 0; i < P2Figures.Length; i++)
-        {
-            Vector3 pos = P2Figures[i].transform.position;
-            pos.x = x_spots[i] * 4;
-            pos.z = z_spots[1] * 4;
-            Quaternion rot = P2Figures[i].transform.rotation;
-            rot.y = 180f;
-            P2Figures[i].transform.position = pos;
-            P2Figures[i].transform.rotation = rot;
-        }
     }
-    void OnButtonClick()
+    public string Figure_Str(bool P1T)
     {
-        //p1turn = !p1turn;
+        if (P1T)
+        {
+            return "Player1Figure";
+        }
+        else
+        {
+            return "Player2Figure";
+        }
     }
-
-    public Collider[] RangeCheck()
+    public Collider[] Movement_RangeCheck()
     {
         if (ChosenFigure != null)
         {
@@ -104,31 +50,53 @@ public class Figure_Movement : MonoBehaviour
             return null;
         }
     }
+    public Collider[] Attack_RangeCheck()
+    {
+        if (ChosenFigure != null)
+        {
+            string FClass = ChosenFigure.GetComponent<FigureScript>().Figure_Class;
+            if (FClass == "Melle")
+            {
+                Collider[] Enemy_Figures = Physics.OverlapSphere(ChosenFigure.transform.position, Radius, LayerMask.GetMask(Figure_Str(Player1Turn)));
+
+            }
+        }
+        return null;
+    }
     void Figure_Chose()
     {
-        Vector3 position = ChosenFigure.transform.position;
-        RaycastHit hit;
-        Physics.Raycast(position, Vector3.down,out hit, 50);
-        //CurrentTile = hit.transform.gameObject;
-        //hex_start = CurrentTile;
-        position.y += 3;
-        outline.OutlineWidth = 3;
-        ChosenFigure.transform.position = position;
-        if (RangeCheck() != null)
+        if (ChosenFigure.GetComponent<FigureScript>().MovementAP == true)
         {
-            foreach (Collider hex in RangeCheck())
+            Vector3 position = ChosenFigure.transform.position;
+            RaycastHit hit;
+            Physics.Raycast(position, Vector3.down, out hit, 50);
+            //CurrentTile = hit.transform.gameObject;
+            //hex_start = CurrentTile;
+            position.y += 3;
+            outline.OutlineWidth = 3;
+            ChosenFigure.transform.position = position;
+            if (Movement_RangeCheck() != null)
             {
-                hex.GetComponent<MeshRenderer>().material = NewMaterial;
-                TileScript tileS = hex.gameObject.GetComponent<TileScript>();
-                tileS.isAvailable = true;
+                foreach (Collider hex in Movement_RangeCheck())
+                {
+                    hex.GetComponent<MeshRenderer>().material = NewMaterial;
+                    TileScript tileS = hex.gameObject.GetComponent<TileScript>();
+                    tileS.isAvailable = true;
+                }
             }
+        }
+        else
+        {
+            Debug.Log("This charecter acted already");
+            ChosenFigure = null;
+            ChosenTile = null;
         }
     }
     void Figure_Move()
     {
-        if (RangeCheck() != null)
+        if (Movement_RangeCheck() != null)
         {
-            foreach (Collider hex in RangeCheck())
+            foreach (Collider hex in Movement_RangeCheck())
             {
                 hex.GetComponent<MeshRenderer>().material = DefaultMaterial;
                 TileScript tileS = hex.gameObject.GetComponent<TileScript>();
@@ -143,43 +111,40 @@ public class Figure_Movement : MonoBehaviour
         figPos.y -= 3;
         ChosenFigure.transform.position = figPos;
         TileScript tileScript = ChosenTile.GetComponent<TileScript>();
-        //tileScript.isTaken = true; 
-        //ChosenFigure.gameobject.SetActive(false);
+        ChosenFigure.GetComponent<FigureScript>().MovementAP = false;
         ChosenFigure = null;
         ChosenTile = null;
     }
     void FigureAttack(GameObject GO1, GameObject GO2)
     {
-        // int Power1 = GO1.GetComponent<FigureScript>().Power;
-        // int Power2 = GO2.GetComponent<FigureScript>().Power;
         int Attacking = GO1.GetComponent<FigureScript>().Power;
-        int Suffering = GO1.GetComponent<FigureScript>().Power;
+        int Suffering = GO2.GetComponent<FigureScript>().Power;
 
         // int Attacking1 = 10; 
         int Throws = Attacking / 4;
-        if (Attacking % 4 > 0){
+        if (Attacking % 4 > 0)
+        {
             Throws++;
         }
-        // Debug.Log(Throws);
+        //Debug.Log(Throws);
 
         int[] Bone = new int[Throws];
         int Damage = 0;
 
         System.Random random = new System.Random();
-        for (int i = 0; i < Throws; i++){
-            Bone[i] = random.Next(1,12);
+        for (int i = 0; i < Throws; i++)
+        {
+            Bone[i] = random.Next(1, 12);
 
-            if (Bone[i] > 6){
+            if (Bone[i] > 6)
+            {
                 Damage++;
             }
-            Debug.Log(Bone[i]);
+            //Debug.Log(Bone[i]);
         }
-        // Debug.Log(Damage);
+        Debug.Log(Damage);
         Suffering -= Damage;
-        // Debug.Log(Suffering);
-
-
-
+        Debug.Log(Suffering);
     }
     void PlayerMove()
     {
@@ -204,7 +169,7 @@ public class Figure_Movement : MonoBehaviour
                     }
                     else if (ChosenFigure != null && !HitGO.CompareTag(ChosenFigure.tag))
                     {
-                        FigureAttack(ChosenFigure, HitGO);
+                        Debug.Log("Attack, don't move");
                     }
                     else
                     {
@@ -236,6 +201,29 @@ public class Figure_Movement : MonoBehaviour
             else
             {
                 Debug.Log("Smth happend");
+            }
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(ray, out hitInfo, 200);
+            if (hit)
+            {
+                GameObject HitGO = hitInfo.transform.gameObject;
+                if (HitGO.CompareTag("Player1Figure") || HitGO.CompareTag("Player2Figure"))
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                        hitInfo = new RaycastHit();
+                        ChosenFigure = hitInfo.transform.gameObject;
+                    }
+                }
+                else
+                {
+                    Debug.Log("Move, not attack 3");
+                }
             }
         }
     }
